ea43472ee6a72de0d3c45398a24ccb97a1a2d7f6
Ananthu Ajayan <137896002+ananthuajayan@users.noreply.github.com>
2024-03-30 11:00:17 +0530

    activity page start

--- src/Components/activity/Activity.js
+++ src/Components/activity/Activity.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Activity.scss'
 
 const Activity = () => {
   return (
-    <div>
-      <h1>welcome to Activity page</h1>
+    <>
+    <div className="activity-head">
+      <h3>Activity Logs</h3>
     </div>
+    <div className="date-search">
+      <div className='dates'>
+      <div className="start-date">
+        <h5>From</h5>
+        <input type="date" />
+      </div>
+      <div className="start-date">
+        <h5>To</h5>
+      <input type="date" />
+      </div>
+      </div>
+      <button>Search</button>
+    
+    </div>
+    </>
   );
 }
 
